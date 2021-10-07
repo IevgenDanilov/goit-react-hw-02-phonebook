f537f9fae4341feb18d53e3f4373b659430f2e74
Yevhen Danilov <danilov.evgen@gmail.com>
2021-10-07 16:59:48 +0300

    2 part of 2 hw compleate upd

---
+++ src/components/contactList/ContactList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ContactList = ({ listContact, onDelete }) => {
+  return (
+    <ul className="contacts">
+      {listContact.map(({ name, id, number }) => {
+        return (
+          <li key={id}>
+            {name}: {number}
+            <button onClick={() => onDelete(id)} type="submit" className="btn">
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
+
+ContactList.propTypes = {
+  listContact: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
